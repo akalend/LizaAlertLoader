cc3116d4846c1d2e4b2114fc78ffc58fb310f911
Alexandre Kalendarev <akalend@mail.ru>
2024-03-24 17:04:10 +0300

    fix server error

--- server/module/route.py
+++ server/module/route.py
@@ -1,14 +1,15 @@
 
 from module.config import conf
+from module import version
+# import module.version
 
 
 def route(config):
 
     config.add_static_view(name='html', path='html')
 
-
-    config.add_route('api', '')
-    # config.add_view(test.echo, route_name='api')
+    config.add_route('api', '/')
+    config.add_view(version.echo, route_name='api')
 
     config.scan()
 
--- server/module/version.py
+++ server/module/version.py
@@ -1,11 +1,7 @@
-import time
+# from module.cache import Cache
+from pyramid.response import Response
 
-from module.cache import Cache
 
-    # print(request.matchdict['id'])
-    # print('mode=' + conf['mode'])
 def echo(request):
 
-    mode = conf['mode']
-    # print(mysql_conf[mode])
-    return Response('TunnelGlobe API V 1.0 <br>https://tunnelglobe.com')
+    return Response('API V 1.0')
